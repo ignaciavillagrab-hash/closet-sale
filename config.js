245c76f47cd6cac5982abaa9ed6667f156ccf573
Maria Ignacia Villagra <ignaciavillagrab-hash@users.noreply.github.com>
2026-07-24 15:01:45 -0400

    Conectar Google Sheet de inventario/ventas

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.SITE_CONFIG = {
   // Pega aquí la URL de "Publicar en la web" de tu Google Sheet (formato CSV).
   // Debe apuntar a la hoja "Inventario y Ventas" en formato CSV.
   // Ejemplo: https://docs.google.com/spreadsheets/d/e/2PACX-XXXXXXX/pub?gid=0&single=true&output=csv
-  SHEET_CSV_URL: "",
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNWirDhxQIM5kblmGVOdx0MLCGhglqhF-V_a2RE-_BCCjDAFHoGOyfR8E8vjmO3A/pub?gid=120620776&single=true&output=csv",
 
   WHATSAPP_NUMBER: "56981365719",
 
